ed8d67f43de4322a1830d90ce1235b4f1c8f2e67
BHAVESH GUPTA <35276600+bhavesh-g@users.noreply.github.com>
2024-02-09 15:26:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/'):
+def index():
+  return render_template('index.html')
+
+@socketio.on('send notification):
+def handle_notification(data):
+  socketio.emit('receive_notification', data)
+  print(data)
+
+if __name__ == '__main__':
+  socketio.run(app, debug=True)
+
